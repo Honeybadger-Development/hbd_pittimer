9efafc4cdd84628e2a3b98783cafe50e30e83224
Greve <frank@corerp.org>
2025-10-19 02:06:14 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ use_experimental_fxv2_oal 'yes'
 name 'hbd_pittimer'
 author 'Honeybadger Development'
 description 'PIT Timer for Police'
-version '1.0.0'
+version '1.0.1'
 
 shared_scripts {
     '@ox_lib/init.lua',
